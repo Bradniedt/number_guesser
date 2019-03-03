13c1f55ea1c8f84ac0c54273907d5e1192a94c92
Brad Niedt <bradleyniedt@gmail.com>
2019-03-02 17:49:26 -0700

    Adds clear number function and started on phase two logic a little early. Will PR then move to phase two branch.

--- js/script.js
+++ js/script.js
@@ -8,6 +8,7 @@ const guessInput = document.getElementById('guessInput');
 const clearGuess = document.getElementById('clearGuess');
 const resetButton = document.getElementById('resetButton');
 
+
 let guessCounter = 1;
 
 
@@ -15,15 +16,22 @@ function evaluateGuess() {
   let userGuess = Number(guessInput.value);
   guessText.textContent = 'Your last guess was:';
   lastGuess.textContent = String(userGuess);
-  if (userGuess === randomNumber) {
-    guessResult.textContent = 'BOOM!';
-    gameOver();
-  } else {
-      if (userGuess < randomNumber) {
-        guessResult.textContent = "That is too low.";
-      } else if (userGuess > randomNumber) {
-        guessResult.textContent = "That is too high.";
+
+  if (userGuess >= 1 && userGuess <= 100) {
+      if (userGuess === randomNumber) {
+        guessResult.textContent = 'BOOM!';
+        gameOver();
+      } else {
+          if (userGuess < randomNumber) {
+            guessResult.textContent = "That is too low.";
+          } else if (userGuess > randomNumber) {
+            guessResult.textContent = "That is too high.";
+          }
       }
+  } else if (Number.isInteger(userGuess)) {
+    alert('You can only enter a guess between 1 and 100! Try again');
+  } else {
+    alert('You can only enter numerical guesses!');
   }
 
 
@@ -37,7 +45,13 @@ function resetGame() {
   guessText.textContent = '';
   lastGuess.textContent = '';
   guessResult.textContent = '';
+  guessInput.textContent = '';
+}
+
+function clearNumber() {
+  guessInput.value = '';
 }
 
 submitGuess.addEventListener('click', evaluateGuess);
 resetButton.addEventListener('click', resetGame);
+clearGuess.addEventListener('click', clearNumber);
